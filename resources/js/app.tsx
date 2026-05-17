942150c5ae2ff61a831e62deec6cc14685d931ce
raihan <r1aihan2003f1irdaus@gmail.com>
2026-05-17 14:16:26 +0700

    refactor: switch from hydrateRoot to createRoot for client-side rendering in app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,7 +2,7 @@ import '../css/app.css';
 
 import { InertiaFlashToasts, ToastProvider } from '@/components/ui/toast';
 import { createInertiaApp } from '@inertiajs/react';
-import { hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -13,8 +13,7 @@ createInertiaApp({
     },
     setup({ el, App, props }) {
         if (el) {
-            hydrateRoot(
-                el,
+            createRoot(el).render(
                 <ToastProvider>
                     <App {...props} />
                     <InertiaFlashToasts />
