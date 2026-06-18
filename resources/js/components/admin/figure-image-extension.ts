2fdb4658411fa5b8fdd0f528e6fcef9fe41c54e8
Raihan Firdaus <95228658+RH203@users.noreply.github.com>
2026-06-18 09:00:48 +0700

    feat(blog): add Tiptap figure image node

---
+++ resources/js/components/admin/figure-image-extension.ts
@@ -0,0 +1,54 @@
+import { mergeAttributes, Node } from '@tiptap/core';
+
+export const FigureImage = Node.create({
+    name: 'figureImage',
+    group: 'block',
+    atom: true,
+    draggable: true,
+    selectable: true,
+
+    addAttributes() {
+        return {
+            src: { default: null },
+            alt: { default: '' },
+            caption: { default: '' },
+            source: { default: '' },
+            size: { default: 'full' },
+            width: { default: null },
+            height: { default: null },
+        };
+    },
+
+    parseHTML() {
+        return [{
+            tag: 'figure[data-type="figure-image"]',
+            getAttrs: (element) => {
+                const figure = element as HTMLElement;
+                const image = figure.querySelector('img');
+                return {
+                    src: image?.getAttribute('src'),
+                    alt: image?.getAttribute('alt') || '',
+                    caption: figure.querySelector('[data-caption]')?.textContent || '',
+                    source: figure.querySelector('[data-source]')?.textContent || '',
+                    size: figure.dataset.size || 'full',
+                    width: image?.getAttribute('width'),
+                    height: image?.getAttribute('height'),
+                };
+            },
+        }];
+    },
+
+    renderHTML({ HTMLAttributes }) {
+        const { src, alt, caption, source, size, width, height } = HTMLAttributes;
+        return [
+            'figure',
+            mergeAttributes({ 'data-type': 'figure-image', 'data-size': size || 'full' }),
+            ['img', { src, alt, width, height, loading: 'lazy', decoding: 'async' }],
+            ['figcaption', {},
+                ['span', { 'data-figure-label': '' }, ''],
+                ['span', { 'data-caption': '' }, caption || ''],
+                ['span', { 'data-source': '' }, source || ''],
+            ],
+        ];
+    },
+});
